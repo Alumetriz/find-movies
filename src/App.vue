f133e31ab3d2aab136f3ee1998261eb9566c4ee1
Alumetriz <fanmax0808@gmail.com>
2023-12-18 18:42:46 +0200

    init: initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {useMovieStore} from "./stores/MovieStore.js";
+
+const store = useMovieStore()
+
+setInterval(() => {
+  store.increment()
+}, 1000)
+
+</script>
+
+<template>
+  <button class="border border-green-400 p-2 rounded-md" @click="store.increment()">Increase</button>
+  <h1 class="text-3xl font-bold text-green-400">{{store.count}}</h1>
+</template>
+
+<style scoped></style>
