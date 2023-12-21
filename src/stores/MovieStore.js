e418570f0597d2ccfa0b7ea4f4afb8cd87ea1d8d
Alumetriz <fanmax0808@gmail.com>
2023-12-21 19:05:13 +0200

    refactor: stores are rewritten to the composition API

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,42 +1,48 @@
 import {defineStore} from "pinia";
+import {computed, reactive, ref} from "vue";
 
-export const useMovieStore = defineStore('movieStore', {
-    state: () => {
-        return {
-            activeTab: 'search',
-            sortValue: 'popular',
-            favoritesSortValue: 'favorites',
-            movies: null,
-        }
-    },
-    actions: {
-        toggleToFavorites(movie) {
-            movie.isFavorite = !movie.isFavorite
-        },
+export const useMovieStore = defineStore('movieStore', () => {
+    const activeTab = ref('search')
+    const sortValue = ref('popular')
+    const favoritesSortValue = ref('favorites')
+    const movies = ref(null)
 
-        toggleToWatched(movie) {
-            movie.isWatched = !movie.isWatched
+    function toggleToFavorites(movie) {
+        movie.isFavorite = !movie.isFavorite
+    }
+
+    function toggleToWatched(movie) {
+        movie.isWatched = !movie.isWatched
+    }
+
+    const filteredMovies = computed(() => {
+        if (!movies.value) return []
+        else {
+            const sortedMovies = [...movies.value.results]
+            return sortValue.value === 'popular'
+                ? sortedMovies.sort((a, b) => b.popularity - a.popularity)
+                : sortedMovies.sort((a, b) => a.popularity - b.popularity)
         }
-    },
-    getters: {
-        filteredMovies: (state) => {
-            if (!state.movies) return [];
-            else {
-                const sortedMovies = [...state.movies.results]
-                console.log(state.movies)
-                return state.sortValue === 'popular'
-                    ? sortedMovies.sort((a, b) => b.popularity - a.popularity)
-                    : sortedMovies.sort((a, b) => a.popularity - b.popularity)
-            }
-        },
-        favoritesMovies: (state) => {
-            if (state.favoritesSortValue === 'favorites') {
-                return state.movies?.results?.filter((movie) => movie.isFavorite)
-            } else if (state.favoritesSortValue === 'watched') {
-                return state.movies?.results.filter((movie) => movie.isFavorite && movie.isWatched)
-            } else if (state.favoritesSortValue === 'not-watched') {
-                return state.movies?.results.filter((movie) => movie.isFavorite && !movie.isWatched)
-            }
+    })
+
+    const favoritesMovies = computed(() => {
+        if (favoritesSortValue.value === 'favorites') {
+            return movies.value?.results?.filter((movie) => movie.isFavorite)
+        } else if (favoritesSortValue.value === 'watched') {
+            return movies.value?.results.filter((movie) => movie.isFavorite && movie.isWatched)
+        } else if (favoritesSortValue.value === 'not-watched') {
+            return movies.value?.results.filter((movie) => movie.isFavorite && !movie.isWatched)
         }
+    })
+
+    return {
+        activeTab,
+        sortValue,
+        favoritesSortValue,
+        movies,
+        toggleToFavorites,
+        toggleToWatched,
+        filteredMovies,
+        favoritesMovies
     }
 })
--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -1,40 +1,42 @@
 import {defineStore} from "pinia";
 import {useMovieStore} from "./MovieStore.js";
+import {ref} from "vue";
 
-export const useSearchStore = defineStore('searchStore', {
-    state: () => {
-        return {
-            loader: false,
-            api_options: {
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGUzY2ZmMTEyNjI3ZjdlYzBkNGZhZGQ1ZGM1MGUwYyIsInN1YiI6IjY1ODA4YWYxZDUxOTFmMDYwNmFkODJiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kjFa056Ks9OvMs_Y_ML4nc8NOIdx7CweaDoUMqUlGsA'
-                }
-            }
+export const useSearchStore = defineStore('searchStore', () => {
+    const loader = ref(false)
+    const api_options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGUzY2ZmMTEyNjI3ZjdlYzBkNGZhZGQ1ZGM1MGUwYyIsInN1YiI6IjY1ODA4YWYxZDUxOTFmMDYwNmFkODJiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kjFa056Ks9OvMs_Y_ML4nc8NOIdx7CweaDoUMqUlGsA'
         }
-    },
-    actions: {
-        async fetchMovies(title) {
-            try {
-                this.loader = true
-                const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=1`, this.api_options)
-                const data = await response.json()
+    }
 
-                data.results = data.results.map((movie) => {
-                    return {
-                        ...movie,
-                        isFavorite: false,
-                        isWatched: false,
-                    }
-                })
-                const movieStore = useMovieStore()
-                movieStore.movies = data
-            } catch (e) {
-                console.log(e)
-            } finally {
-                this.loader = false
-            }
-        },
+    async function fetchMovies(title) {
+        try {
+            loader.value = true
+            const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=1`, api_options)
+            const data = await response.json()
+
+            data.results = data.results.map((movie) => {
+                return {
+                    ...movie,
+                    isFavorite: false,
+                    isWatched: false
+                }
+            })
+
+            const movieStore = useMovieStore()
+            console.log(movieStore.movies)
+            movieStore.movies = data
+        } catch (e) {
+            console.log(e)
+        } finally {
+            loader.value = false
+        }
+    }
+
+    return {
+        loader, fetchMovies
     }
 })
